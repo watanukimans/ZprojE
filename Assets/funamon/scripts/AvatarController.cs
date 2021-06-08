199996bfe1838ec08bac27918f1dd5cb8f0e12e5
RyutaroFunamoto <ryu9asa@icloud.com>
2021-06-09 01:18:18 +0900

    ネットワーク関連のテストシーンを追加しました。全て『funamon』フォルダにあります。もしアセットとかでエラー起きたら言って〜

---
+++ Assets/funamon/scripts/AvatarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AvatarController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //自身が生成したオブジェクトのみ移動処理をする
+        if (photonView.IsMine)
+        {
+            var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+            transform.Translate(6f * Time.deltaTime * input.normalized);
+        }
+    }
+}
---
+++ Assets/funamon/scripts/TestScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+// MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
+public class TestScene : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.NickName = "Player";
+
+        // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster()
+    {
+        // "TestRoom"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
+        PhotonNetwork.JoinOrCreateRoom("TestRoom", new RoomOptions(), TypedLobby.Default);
+        //base.OnConnectedToMaster();
+    }
+
+    // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom()
+    {
+        // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
+        var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
+        //base.OnJoinedRoom();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
