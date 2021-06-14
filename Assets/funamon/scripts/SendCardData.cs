5be904ab922365060d78ff843f55370bc7572fd5
RyutaroFunamoto <ryu9asa@icloud.com>
2021-06-14 17:59:15 +0900

    ネットの続き

---
+++ Assets/funamon/scripts/SendCardData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendCardData : MonoBehaviour
+{
+    GameObject MainBrain; //自身のネットワークオブジェクトを格納
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainBrain = TestScene.My;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click1() //カードをクリックする
+    {
+        MainBrain.SendMessage("Select", this.gameObject.name);
+    }
+}
