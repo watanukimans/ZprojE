af5d7804cb6984ec412e3d1d4fcf18639b634706
watanuki <im0178sf@ed.ritsumei.ac.jp>
2021-06-14 17:29:31 +0900

    ババ抜きのルール関連部分途中経過

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] CardController cardPrefab;
+    [SerializeField] Transform playerHand,EnemyHand;
+
+    bool isPlayerTurn = true; 
+    List<int> deck = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+    List<int> deck2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    public CardEntity cardEntity;
+    
+
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+        // 初期手札を配る
+        SetStartHand();
+
+        // ターンの決定
+        TurnCalc();
+    }
+
+    public void CreateCard(int cardID, Transform place)
+    {
+        CardController card = Instantiate(cardPrefab, place);
+        card.Init(cardID);
+        //お試し用4行
+        if (cardID == 1)
+        {
+            card.tag = "Card1";
+        }
+        if (cardID == 2)
+        {
+            card.tag = "Card2";
+        }
+        if (cardID == 3)
+        {
+            card.tag = "Card3";
+        }
+        if (cardID == 4)
+        {
+            card.tag = "Card4";
+        }
+        if (cardID == 5)
+        {
+            card.tag = "Card5";
+        }
+        if (cardID == 6)
+        {
+            card.tag = "Card6";
+        }
+        if (cardID == 7)
+        {
+            card.tag = "Card7";
+        }
+        if (cardID == 8)
+        {
+            card.tag = "Card8";
+        }
+        if (cardID == 9)
+        {
+            card.tag = "Card9";
+        }
+        if (cardID == 10)
+        {
+            card.tag = "Card10";
+        }
+    }
+    public void CreateCard2(int cardID, Transform place)
+    {
+        CardController card = Instantiate(cardPrefab, place);
+        card.Initura(cardID);
+        if (cardID == 1)
+        {
+            card.tag = "Card1";
+        }
+        if (cardID == 2)
+        {
+            card.tag = "Card2";
+        }
+        if (cardID == 3)
+        {
+            card.tag = "Card3";
+        }
+        if (cardID == 4)
+        {
+            card.tag = "Card4";
+        }
+        if (cardID == 5)
+        {
+            card.tag = "Card5";
+        }
+        if (cardID == 6)
+        {
+            card.tag = "Card6";
+        }
+        if (cardID == 7)
+        {
+            card.tag = "Card7";
+        }
+        if (cardID == 8)
+        {
+            card.tag = "Card8";
+        }
+        if (cardID == 9)
+        {
+            card.tag = "Card9";
+        }
+        if (cardID == 10)
+        {
+            card.tag = "Card10";
+        }
+    }
+    void DrowCard(Transform hand) // カードを引く
+    {
+        // デッキがないなら引かない
+        if (deck.Count == 0)
+        {
+            return;
+        }
+
+        // デッキの一番上のカードを抜き取り、手札に加える
+        int cardID = deck[0];
+        deck.RemoveAt(0);
+        CreateCard(cardID, hand);
+    }
+    void DrowCard2(Transform hand) // カードを引く
+    {
+        // デッキがないなら引かない
+        if (deck2.Count == 0)
+        {
+            return;
+        }
+
+        // デッキの一番上のカードを抜き取り、手札に加える
+        int cardID = deck2[0];
+        deck2.RemoveAt(0);
+        CreateCard(cardID, hand);
+    }
+    void DrowCard3(Transform hand) // カードを引く
+    {
+        // デッキがないなら引かない
+        if (deck.Count == 0)
+        {
+            return;
+        }
+
+        // デッキの一番上のカードを抜き取り、手札に加える
+        int cardID = deck[0];
+        deck.RemoveAt(0);
+        CreateCard2(cardID, hand);
+    }
+    void DrowCard4(Transform hand) // カードを引く
+    {
+        // デッキがないなら引かない
+        if (deck2.Count == 0)
+        {
+            return;
+        }
+
+        // デッキの一番上のカードを抜き取り、手札に加える
+        int cardID = deck2[0];
+        deck2.RemoveAt(0);
+        CreateCard2(cardID, hand);
+    }
+    void SetStartHand() // 手札を配る
+    {
+        Shuffle();
+        int decknum = Random.Range(1, 3);
+        switch (decknum)
+        {
+            case 1:
+                for (int i = 0; i < 11; i++)
+                {
+                    DrowCard(playerHand);
+                }
+                for (int i = 0; i < 11; i++)
+                {
+                    DrowCard4(EnemyHand);
+                }
+                break;
+            case 2:
+                for (int i = 0; i < 11; i++)
+                {
+                    DrowCard2(playerHand);
+                }
+                for (int i = 0; i < 11; i++)
+                {
+                    DrowCard3(EnemyHand);
+                }
+                break;
+        }
+    }
+    void TurnCalc() // ターンを管理する
+    {
+        if (isPlayerTurn)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+    public void ChangeTurn() // ターンエンド処理
+    {
+        isPlayerTurn = !isPlayerTurn; // ターンを逆にする
+        TurnCalc(); // ターンを相手に回す
+    }
+    void PlayerTurn()
+    {
+        Debug.Log("Playerのターン");
+    }
+
+    void EnemyTurn()
+    {
+        Debug.Log("Enemyのターン");
+        ChangeTurn(); // ターンエンドする
+    }
+    void Shuffle ()
+    {
+        int n = deck.Count;
+        while(n>1)
+        { n--;
+            int k = UnityEngine.Random.Range(0, n + 1);
+            int temp = deck[k];
+            deck[k] = deck[n];
+            deck[n] = temp;
+        }
+        int m = deck2.Count;
+        while (m > 1)
+        {
+            m--;
+            int l = UnityEngine.Random.Range(0, m + 1);
+            int temp2 = deck2[l];
+           deck2[l] = deck2[m];
+             deck2[m] = temp2;
+        }
+    }
+}
---
+++ Assets/Scripts/OnClickS.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnClickS : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] GameObject panel;
+    public Image cardImage;
+    public Sprite ace;
+    public Sprite two;
+    public Sprite three;
+    public Sprite four;
+    public Sprite five;
+    public Sprite six;
+    public Sprite seven;
+    public Sprite eight;
+    public Sprite nine;
+    public Sprite joker;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Y座標を移動
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+        pos.y -= 200f;
+        myTransform.position = pos;
+        // クリックされた時に行いたい処理
+        Debug.Log("押されたよ");
+        if (this.gameObject.CompareTag("Card1"))
+        {
+            cardImage.sprite = ace;
+            //Destroy(this.gameObject);
+            GameObject[] Card1 = GameObject.FindGameObjectsWithTag("Card1");
+            foreach (GameObject card1 in Card1)
+                GameObject.Destroy(card1,2.0f);
+        }
+        if (this.gameObject.CompareTag("Card2"))
+        {
+            cardImage.sprite = two;
+            //Destroy(this.gameObject);
+            GameObject[] Card2 = GameObject.FindGameObjectsWithTag("Card2");
+            foreach (GameObject card2 in Card2)
+                GameObject.Destroy(card2, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card3"))
+        {
+            cardImage.sprite = three;
+            //Destroy(this.gameObject);
+            GameObject[] Card3 = GameObject.FindGameObjectsWithTag("Card3");
+            foreach (GameObject card3 in Card3)
+                GameObject.Destroy(card3, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card4"))
+        {
+            cardImage.sprite = four;
+            //Destroy(this.gameObject);
+            GameObject[] Card4 = GameObject.FindGameObjectsWithTag("Card4");
+            foreach (GameObject card4 in Card4)
+                GameObject.Destroy(card4, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card5"))
+        {
+            cardImage.sprite = five;
+            //Destroy(this.gameObject);
+            GameObject[] Card5 = GameObject.FindGameObjectsWithTag("Card5");
+            foreach (GameObject card5 in Card5)
+                GameObject.Destroy(card5, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card6"))
+        {
+            cardImage.sprite = six;
+            //Destroy(this.gameObject);
+            GameObject[] Card6 = GameObject.FindGameObjectsWithTag("Card6");
+            foreach (GameObject card6 in Card6)
+                GameObject.Destroy(card6, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card7"))
+        {
+            cardImage.sprite = seven;
+            //Destroy(this.gameObject);
+            GameObject[] Card7 = GameObject.FindGameObjectsWithTag("Card7");
+            foreach (GameObject card7 in Card7)
+                GameObject.Destroy(card7, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card8"))
+        {
+            cardImage.sprite = eight;
+            //Destroy(this.gameObject);
+            GameObject[] Card8 = GameObject.FindGameObjectsWithTag("Card8");
+            foreach (GameObject card8 in Card8)
+                GameObject.Destroy(card8, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card9"))
+        {
+            cardImage.sprite = nine;
+            //Destroy(this.gameObject);
+            GameObject[] Card9 = GameObject.FindGameObjectsWithTag("Card9");
+            foreach (GameObject card9 in Card9)
+                GameObject.Destroy(card9, 2.0f);
+        }
+        if (this.gameObject.CompareTag("Card10"))
+        {
+            cardImage.sprite = joker;
+            GameObject[] Card10 = GameObject.FindGameObjectsWithTag("Card10");
+            foreach (GameObject card10 in Card10)
+                GameObject.Destroy(card10, 2.0f);
+        }
+    }
+}
